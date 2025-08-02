bf1179b983951f0fc57102ca1674b845d7f0482c
DerJohnBR <derjohnbr@gmail.com>
2025-08-01 21:43:14 -0300

    Criado arquivos de Declaração de Variáveis

---
+++ 02_Variaveis/declaracaoVariaveis.cs
@@ -0,0 +1,28 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	static void Main(){
+		/* 
+		Declaração de Variável
+		*/
+		int a;
+		// Inicialização da Variável
+		a = 22;
+		Console.WriteLine(a);
+		// Reatribuição de Valores na Variável
+		a = 56;
+		Console.WriteLine(a);
+		
+		// Outras formas de declaração de Variável
+		
+		// Declarando e Inicializando a Variável
+		int b = 925;
+		Console.WriteLine(b);
+		
+		int y = 10, x = 20;
+		int z = y + x;
+		Console.WriteLine(z);
+		
+	}
+}
