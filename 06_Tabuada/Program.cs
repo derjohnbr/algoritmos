b14016a94d2307fa6623f2ff293df92c79ca805a
DerJohnBR <derjohnbr@gmail.com>
2025-08-08 22:01:51 -0300

    Exercício de Criação Tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string continuar = "S";
+
+            while (continuar.ToUpper() == "S") {
+                Console.WriteLine("Digite um Número");
+                numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nTabuada do {0}\n", numero);
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} x {1} = {2}", i, numero, numero * i);
+                }
+
+                Console.WriteLine("\nDeseja continuar (S/N)");
+                continuar = Console.ReadLine();
+
+            }
+        }
+    }
+}
