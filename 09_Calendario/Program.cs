7ffd28179e6ab923e2531b2ec49085ff7f79ced6
DerJohnBR <derjohnbr@gmail.com>
2025-08-25 21:06:42 -0300

    Atualizado Calendário incluindo Feriados

--- 09_Calendario/Program.cs
+++ 09_Calendario/Program.cs
@@ -45,19 +45,66 @@ namespace _09_Calendario
                 }
             }
 
-            Console.WriteLine($"Canlendário de {primeiroDiaMes.ToString("MMMM")} de {ano}");
-            Console.WriteLine($"Dom\tSeg\tTer\tQua\tQui\tSex\tSab");
+            Console.WriteLine($"\nCanlendário de {primeiroDiaMes.ToString("MMMM")} de {ano}");
+            Console.WriteLine($"\nDom\tSeg\tTer\tQua\tQui\tSex\tSab");
+
+            int[] diasFeriados = RetornaFeriados(mes, ano);
+            
             //Imprimir o calendário
             for (int semana = 0; semana < 6; semana++)
             {
                 for (int diaSemana = 0; diaSemana < 7; diaSemana++)
                 {
+                    if(diasFeriados.Contains(calendario[semana, diaSemana]) || diaSemana == 0)
+                        Console.ForegroundColor = ConsoleColor.Red;
+
                     int diaCalendario = calendario[semana, diaSemana];
                     Console.Write((diaCalendario == 0 ? "  " : diaCalendario.ToString("00")) + "\t");
+                    
+                    Console.ResetColor();
                 }
                 Console.WriteLine();
             }
+
+            int[] feriados = RetornaFeriados(mes, ano);
+            Console.Write($"\nFeriados: ");
+            for (int i = 0; i < feriados.Length; i++)
+            {
+                if (feriados[i] != 0)
+                    Console.Write(feriados[i].ToString("00") + "\t");
+            }
+
             Console.ReadKey();
+        }
+
+        public static int[] RetornaFeriados(int mes, int ano)
+        {
+            int[] feriados = new int[15];
+            int indice = 0;
+            //feriados[indice++] = 11;
+            //feriados[indice++] = 21;
+            if (mes == 1)
+                feriados[indice++] = 1; //Confraternização Universal
+            else if (mes == 4)
+                feriados[indice++] = 21; //Tiradentes
+            else if (mes == 5)
+                feriados[indice++] = 1; //Dia do Trabalho
+            else if (mes == 7)
+                feriados[indice++] = 9; 
+            else if (mes == 9)
+                feriados[indice++] = 7; //Independência do Brasil
+            else if (mes == 10)
+                feriados[indice++] = 12; //Nossa Senhora Aparecida
+            else if (mes == 11)
+            {
+                feriados[indice++] = 2; //Finados
+                feriados[indice++] = 15; //Proclamação da República
+                feriados[indice++] = 20; //Consciência Negra
+            }
+            else if (mes == 12)
+                feriados[indice++] = 25; //Natal
+
+            return feriados;
         }
     }
 }
